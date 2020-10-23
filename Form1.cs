8db54b92631ac758f523ae0d951468ee29bcc949
Will <37053782+williamDunn@users.noreply.github.com>
2020-10-23 03:45:21 -0500

    Add files via upload

---
+++ Form1.cs
@@ -0,0 +1,60 @@
+﻿//William Dunn - 285 - A10 - 4/12/18
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace A10_DUNNW
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void openForm()
+        {
+            Form2 f2 = new Form2();
+            f2.ShowDialog();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnMovie_Click(object sender, EventArgs e)
+        {
+            openForm();
+            using (Form2 form2 = new Form2())
+            {
+                form2.crystalReport = "movieReport";
+            }
+        }
+
+        private void btnCompany_Click(object sender, EventArgs e)
+        {
+            openForm();
+            using (Form2 form2 = new Form2())
+            {
+                form2.crystalReport = "companyReport";
+            }
+        }
+
+        private void btnStar_Click(object sender, EventArgs e)
+        {
+            openForm();
+            using (Form2 form2 = new Form2())
+            {
+                form2.crystalReport = "starReport";
+            }
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,43 @@
+﻿//William Dunn - 285 - A10 - 4/12/18
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CrystalDecisions.CrystalReports.Engine;
+
+namespace A10_DUNNW
+{
+    public partial class Form2 : Form
+    {
+
+        public Form2()
+        {
+            InitializeComponent();
+
+            if (crystalReport == "movieReport")
+            {
+                crystalReportViewer1.ReportSource = MovieReport1;
+                crystalReportViewer1.Refresh();
+            }
+            else if(crystalReport == "companyReport")
+            {
+                crystalReportViewer1.ReportSource = ProductionCompanyReport1;
+                crystalReportViewer1.Refresh();
+            }
+            else if(crystalReport == "starReport")
+            {
+                crystalReportViewer1.ReportSource = StarReport1;
+                crystalReportViewer1.Refresh();
+            }
+        }
+
+        public string crystalReport;
+
+    }
+}
